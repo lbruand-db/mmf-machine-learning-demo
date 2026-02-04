27ae4e2a23110005042b0f8d68145ee93aab2812
lbruand-db <lucas.bruand+github@databricks.com>
2026-02-04 17:49:29 +0100

    revert base url

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.BASE_URL || '/',
+  base: 'https://lbruand-db.github.io/mmf-machine-learning-demo/',
 })
